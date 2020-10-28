739172be553a87830870a63728aff3e96be23fcc
Anton <ton_vampire@mail.ru>
2020-10-28 12:02:58 +0300

    changed styles

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <div class="block">
-      <Navigation />
+      <Navigation/>
       <select name="lang" class="block__lang" v-model="defaultLang">
         <option
           v-for="(item, i) in langList"
@@ -12,29 +12,31 @@
       </select>
     </div>
     <div class="main">
-      <div class="header">
-        <h2 class="header__name"><span class="msg">{{ msg }}</span></h2>
-        <select name="city" class="header__city">
-          <option
-            v-for="(item, i) in cityList"
-            :key="i"
-          >
-            {{ item }}
-          </option>
-        </select>
-      </div>
-      <div class="body">
-        <h1>Каршеринг <span class="msg">{{ msg }}</span></h1>
-        <p>Поминутная аренда авто твоего города</p>
-        <v-btn
-          elevation="2"
-          color="#0EC261"
-        >Забронировать
-        </v-btn>
-      </div>
-      <div class="footer">
-        <p class="footer__info">2016-2019 "{{ msg }}"</p>
-        <p class="footer__number">8 (495) 234-22-44</p>
+      <div class="main__content">
+        <div class="header">
+          <h2 class="header__name"><span class="msg">{{ msg }}</span></h2>
+          <select name="city" class="header__city">
+            <option
+              v-for="(item, i) in cityList"
+              :key="i"
+            >
+              {{ item }}
+            </option>
+          </select>
+        </div>
+        <div class="body">
+          <h1>Каршеринг <span class="msg">{{ msg }}</span></h1>
+          <p>Поминутная аренда авто твоего города</p>
+          <v-btn
+            elevation="2"
+            color="#0EC261"
+          >Забронировать
+          </v-btn>
+        </div>
+        <div class="footer">
+          <p class="footer__info">2016-2019 "{{ msg }}"</p>
+          <p class="footer__number">8 (495) 234-22-44</p>
+        </div>
       </div>
     </div>
     <carousel/>
@@ -68,75 +70,77 @@ export default {
 
 .home {
   display: flex;
-}
 
-.main {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  min-height: 100%;
-  min-width: calc(50% - 64px);
-}
-
-.msg {
-  color: green;
-}
-
-.block {
-  display: inline-flex;
-  flex-direction: column;
-  justify-content: space-between;
-  background-color: black;
-  height: 100vh;
-  width: 64px;
-  &__link {
-    color: #0EC261;
-  }
-  &__lang {
-    color: #0EC261;
-  }
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  height: 80px;
-  width: 100%;
-}
-
-.body {
-  display: inline-flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  h1 {
-    font-size: 70px;
+  .block {
     display: flex;
-    align-self: flex-start;
-    flex-wrap: wrap;
-  }
+    flex-direction: column;
+    justify-content: space-between;
+    background-color: black;
+    width: 5%;
 
-  p {
-    display: flex;
-    align-self: flex-start;
-    font-size: 26px;
-    color: gray;
-  }
+    .nav_container {
+      margin-top: 34px;
+    }
 
-  .v-btn {
-    display: flex;
-    align-self: flex-start;
-    &__content {
-      color: white;
+    &__lang {
+      color: #0EC261;
+      padding: 0 5px 37px 5px;
     }
   }
-}
 
-.footer {
-  display: flex;
-  align-items: flex-end;
-  justify-content: space-around;
+  .msg {
+    color: #0EC261;
+  }
+
+  .main {
+    min-width: 45%;
+    &__content {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      min-height: 100%;
+      padding: 5%;
+    }
+
+    .header {
+      display: flex;
+      justify-content: space-between;
+      height: 10%;
+      width: 100%;
+    }
+
+    .body {
+      display: inline-flex;
+      flex-direction: column;
+      justify-content: center;
+
+      h1 {
+        font-size: 70px;
+        display: flex;
+        flex-wrap: wrap;
+        line-height: 1;
+      }
+
+      p {
+        font-size: 26px;
+        color: #999999;
+        margin-top: 30px;
+      }
+
+      .v-btn {
+        width: 250px;
+        margin-top: 60px;
+        &__content {
+          color: white;
+        }
+      }
+    }
+
+    .footer {
+      display: flex;
+      justify-content: space-between;
+    }
+  }
 }
 
 </style>
